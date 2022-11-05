66393a677d7aea6427b782ba5f43dd0c127a85b2
hyejungg <kimhyejung12@naver.com>
2022-11-05 17:10:29 +0900

    #3 [fix] 회원가입이 안된 유저라면 예외를 던지도록 Utils 함수 수정

--- src/main/java/com/toursix/turnaround/service/user/UserServiceUtils.java
+++ src/main/java/com/toursix/turnaround/service/user/UserServiceUtils.java
@@ -1,12 +1,13 @@
 package com.toursix.turnaround.service.user;
 
 import static com.toursix.turnaround.common.exception.ErrorCode.CONFLICT_USER_EXCEPTION;
+import static com.toursix.turnaround.common.exception.ErrorCode.NOT_FOUND_USER_EXCEPTION;
+
 import com.toursix.turnaround.common.exception.ConflictException;
 import com.toursix.turnaround.common.exception.NotFoundException;
 import com.toursix.turnaround.domain.user.User;
 import com.toursix.turnaround.domain.user.UserSocialType;
 import com.toursix.turnaround.domain.user.repository.UserRepository;
-import com.toursix.turnaround.common.exception.ErrorCode;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -20,8 +21,14 @@ public class UserServiceUtils {
         }
     }
 
-    public static User findUserBySocialIdAndSocialType(UserRepository userRepository, String socialId, UserSocialType socialType) {
-        return userRepository.findUserBySocialIdAndSocialType(socialId, socialType);
+    public static User findUserBySocialIdAndSocialType(UserRepository userRepository, String socialId,
+            UserSocialType socialType) {
+        User user = userRepository.findUserBySocialIdAndSocialType(socialId, socialType);
+        if (user == null) {
+            throw new NotFoundException(String.format("존재하지 않는 유저 (%s) (%s) 입니다", socialType, socialId),
+                    NOT_FOUND_USER_EXCEPTION);
+        }
+        return user;
     }
 
     public static User findUserById(UserRepository userRepository, Long userId) {
