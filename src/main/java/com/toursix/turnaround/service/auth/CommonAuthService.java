8417843822cff8a2097d5d4c04153f1e17bb0968
Joon <orijoon98@gmail.com>
2022-11-09 01:17:24 +0900

    #12 [fix] 로그인 fcmToken 중복 관련 트랜잭션 관련 이슈 해결

--- src/main/java/com/toursix/turnaround/service/auth/CommonAuthService.java
+++ src/main/java/com/toursix/turnaround/service/auth/CommonAuthService.java
@@ -6,6 +6,7 @@ import com.toursix.turnaround.domain.user.repository.UserRepository;
 import com.toursix.turnaround.service.user.UserServiceUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
@@ -20,5 +21,14 @@ public class CommonAuthService {
         User user = UserServiceUtils.findUserById(userRepository, userId);
         jwtProvider.expireRefreshToken(user.getId());
         user.resetFcmToken();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void resetConflictFcmToken(String fcmToken) {
+        User conflictFcmTokenUser = userRepository.findUserByFcmToken(fcmToken);
+        if (conflictFcmTokenUser != null) {
+            jwtProvider.expireRefreshToken(conflictFcmTokenUser.getId());
+            conflictFcmTokenUser.resetFcmToken();
+        }
     }
 }
