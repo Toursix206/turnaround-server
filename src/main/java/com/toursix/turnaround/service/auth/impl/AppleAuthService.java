cd90f363742da1ea635a9064856b2cd1dbfd26f4
Joon <orijoon98@gmail.com>
2022-11-16 12:34:39 +0900

    #53 [refactor] validate 관련 유틸 분리

--- src/main/java/com/toursix/turnaround/service/auth/impl/AppleAuthService.java
+++ src/main/java/com/toursix/turnaround/service/auth/impl/AppleAuthService.java
@@ -8,6 +8,7 @@ import com.toursix.turnaround.external.client.apple.AppleTokenProvider;
 import com.toursix.turnaround.service.auth.AuthService;
 import com.toursix.turnaround.service.auth.CommonAuthService;
 import com.toursix.turnaround.service.auth.CommonAuthServiceUtils;
+import com.toursix.turnaround.service.auth.CommonAuthValidateUtils;
 import com.toursix.turnaround.service.auth.dto.request.LoginDto;
 import com.toursix.turnaround.service.auth.dto.request.SignUpDto;
 import com.toursix.turnaround.service.user.UserService;
@@ -44,7 +45,7 @@ public class AppleAuthService implements AuthService {
         User user = UserServiceUtils.findUserBySocialIdAndSocialType(userRepository, socialId,
                 socialType);
         commonAuthService.resetConflictFcmToken(request.getFcmToken());
-        CommonAuthServiceUtils.validateUniqueLogin(redisTemplate, user);
+        CommonAuthValidateUtils.validateUniqueLogin(redisTemplate, user);
         user.updateFcmToken(request.getFcmToken());
         return user.getId();
     }
