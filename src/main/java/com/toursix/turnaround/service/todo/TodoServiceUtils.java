1b76bbd77faa5354192636378a5c56b2de18b417
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-19 20:37:06 +0900

    #67 [fix] 코드 리뷰 반영

--- src/main/java/com/toursix/turnaround/service/todo/TodoServiceUtils.java
+++ src/main/java/com/toursix/turnaround/service/todo/TodoServiceUtils.java
@@ -10,13 +10,24 @@ import com.toursix.turnaround.domain.todo.repository.TodoRepository;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TodoServiceUtils {
+
+    private static final String SECONDS = "seconds";
+    private static final String MINUTES = "minutes";
+    private static final String HOURS = "hours";
+    private static final String DAYS = "days";
+    private static final String GET_REWARD = "눌러서 리워드 받기";
+    private static final String UNTIL_TODAY = "오늘까지";
+    private static final String START_NOW = "지금 시작하세요!";
+    private static final String D_DAY = "D-";
 
     public static Todo findTodoById(TodoRepository todoRepository, Long todoId) {
         Todo todo = todoRepository.findTodoById(todoId);
@@ -70,39 +81,51 @@ public class TodoServiceUtils {
 
     public static String todoInfoLeftTime(LocalDateTime now, Todo todo) {
         if (todo.getStage() == TodoStage.SUCCESS) {
-            return "눌러서 리워드 받기";
+            return GET_REWARD;
         }
         if (now.isAfter(todo.getEndAt())) {
-            return "오늘까지";
+            return UNTIL_TODAY;
         }
         if (now.isAfter(todo.getStartAt())) {
-            return "지금 시작하세요!";
+            return START_NOW;
         }
-        Duration duration = Duration.between(now, todo.getStartAt());
-        long seconds = duration.getSeconds();
-        long minutes = seconds / 60;
-        long hours = minutes / 60;
-        long days = hours / 24;
+        Map<String, Long> time = calculateTime(Duration.between(now, todo.getStartAt()));
+        long seconds = time.get(SECONDS);
+        long minutes = time.get(MINUTES);
+        long hours = time.get(HOURS);
+        long days = time.get(DAYS);
         if (days == 0) {
             return String.format("%02d:%02d:%02d", hours, minutes % 60, seconds % 60);
         }
         Period period = Period.between(now.toLocalDate(), todo.getStartAt().toLocalDate());
-        return "D-" + period.getDays();
+        return D_DAY + period.getDays();
     }
 
     public static String todoDetailInfoLeftTime(LocalDateTime now, Todo todo) {
         if (now.isAfter(todo.getStartAt())) {
             return "0시간 0분";
         }
-        Duration duration = Duration.between(now, todo.getStartAt());
-        long seconds = duration.getSeconds();
-        long minutes = seconds / 60;
-        long hours = minutes / 60;
-        long days = hours / 24;
+        Map<String, Long> time = calculateTime(Duration.between(now, todo.getStartAt()));
+        long minutes = time.get(MINUTES);
+        long hours = time.get(HOURS);
+        long days = time.get(DAYS);
         if (days == 0) {
             return String.format("%d시간 %d분", hours, minutes % 60);
         }
         Period period = Period.between(now.toLocalDate(), todo.getStartAt().toLocalDate());
-        return "D-" + period.getDays();
+        return D_DAY + period.getDays();
+    }
+
+    private static Map<String, Long> calculateTime(Duration duration) {
+        Map<String, Long> time = new HashMap<>();
+        long seconds = duration.getSeconds();
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+        long days = hours / 24;
+        time.put(SECONDS, seconds);
+        time.put(MINUTES, minutes);
+        time.put(HOURS, hours);
+        time.put(DAYS, days);
+        return time;
     }
 }
