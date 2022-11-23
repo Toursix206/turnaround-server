3dcefdb2f7905d7fb75f39bc0d740bd5f09d5262
hyejungg <kimhyejung12@naver.com>
2022-11-23 14:47:04 +0900

    #68 [fix] 활동 가이드 조회 시 해당 활동 Id를 받아 검사하도록 로직 수정

--- src/main/java/com/toursix/turnaround/domain/todo/repository/TodoRepositoryCustom.java
+++ src/main/java/com/toursix/turnaround/domain/todo/repository/TodoRepositoryCustom.java
@@ -1,6 +1,5 @@
 package com.toursix.turnaround.domain.todo.repository;
 
-import com.toursix.turnaround.domain.activity.Activity;
 import com.toursix.turnaround.domain.todo.Todo;
 import com.toursix.turnaround.domain.user.Onboarding;
 import java.time.LocalDateTime;
@@ -10,6 +9,4 @@ public interface TodoRepositoryCustom {
     Todo findTodoById(Long todoId);
 
     boolean existsByOnboardingAndStartAtAndEndAt(Onboarding onboarding, LocalDateTime startAt, LocalDateTime endAt);
-
-    boolean existsByOnboardingAndActivityStartAt(Onboarding onboarding, Activity activity, LocalDateTime startAt);
 }
--- src/main/java/com/toursix/turnaround/service/todo/TodoRetrieveService.java
+++ src/main/java/com/toursix/turnaround/service/todo/TodoRetrieveService.java
@@ -1,10 +1,19 @@
 package com.toursix.turnaround.service.todo;
 
+import static com.toursix.turnaround.common.exception.ErrorCode.NOT_FOUND_ACTIVITY_GUIDE_EXCEPTION;
+
+import com.toursix.turnaround.common.exception.NotFoundException;
 import com.toursix.turnaround.common.util.DateUtils;
+import com.toursix.turnaround.domain.activity.Activity;
+import com.toursix.turnaround.domain.activity.ActivityGuide;
+import com.toursix.turnaround.domain.activity.repository.ActivityRepository;
 import com.toursix.turnaround.domain.todo.Todo;
 import com.toursix.turnaround.domain.todo.repository.TodoRepository;
+import com.toursix.turnaround.domain.user.Onboarding;
 import com.toursix.turnaround.domain.user.User;
 import com.toursix.turnaround.domain.user.repository.UserRepository;
+import com.toursix.turnaround.service.activity.dto.response.ActivityGuideResponse;
+import com.toursix.turnaround.service.activity.dto.response.ActivityGuideResponse.ActivityGuideInfo;
 import com.toursix.turnaround.service.todo.dto.response.TodoInfoResponse;
 import com.toursix.turnaround.service.todo.dto.response.TodoMainResponse;
 import com.toursix.turnaround.service.user.UserServiceUtils;
@@ -21,6 +30,7 @@ public class TodoRetrieveService {
 
     private final UserRepository userRepository;
     private final TodoRepository todoRepository;
+    private final ActivityRepository activityRepository;
 
     public TodoMainResponse getTodoMainInfo(Long userId) {
         User user = UserServiceUtils.findUserById(userRepository, userId);
@@ -39,5 +49,23 @@ public class TodoRetrieveService {
         LocalDateTime now = DateUtils.todayLocalDateTime();
         Todo todo = TodoServiceUtils.findTodoById(todoRepository, todoId);
         return TodoInfoResponse.of(now, todo);
+    }
+
+    public ActivityGuideResponse getTodoGuide(Long todoId, Long userId) {
+        User user = UserServiceUtils.findUserById(userRepository, userId);
+        Onboarding onboarding = user.getOnboarding();
+        Todo todo = TodoServiceUtils.findTodoById(todoRepository, todoId);
+        Activity activity = todo.getActivity();
+        LocalDateTime now = DateUtils.todayLocalDateTime();
+        if (now.isBefore(todo.getStartAt())) {
+            TodoValidateUtils.validateUniqueTodoTime(todoRepository, onboarding, now,
+                    now.plusMinutes(activity.getDuration()));
+        }
+        List<ActivityGuide> activityGuide = activityRepository.findActivityGuidesByActivity(activity);
+        if (activityGuide.isEmpty()) {
+            throw new NotFoundException(String.format("존재하지 않는 활동 가이드 (%s) 입니다", activity.getId()),
+                    NOT_FOUND_ACTIVITY_GUIDE_EXCEPTION);
+        }
+        return ActivityGuideResponse.of(activity.getId(), activity.getName(), ActivityGuideInfo.of(activityGuide));
     }
 }
