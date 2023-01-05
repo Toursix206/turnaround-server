95ff941e9d58bd7036eb890089336eaaab7bb216
hyejungg <kimhyejung12@naver.com>
2023-01-05 23:09:01 +0900

    #92 [fix] review 조회 시 score 밖으로 빼고, swagger 수정

--- src/main/java/com/toursix/turnaround/service/todo/dto/response/DoneReviewInfoResponse.java
+++ src/main/java/com/toursix/turnaround/service/todo/dto/response/DoneReviewInfoResponse.java
@@ -1,8 +1,8 @@
 package com.toursix.turnaround.service.todo.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.toursix.turnaround.common.util.DateUtils;
 import com.toursix.turnaround.domain.todo.DoneReview;
-import com.toursix.turnaround.domain.todo.DoneReviewRating;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,17 +20,22 @@ public class DoneReviewInfoResponse {
     private String name;
     private String imageUrl;
     private String doneDate;
-    private DoneReviewRating rating;
+    private int score;
     private String content;
     private boolean isWritten;
     private int point;
+
+    @JsonProperty("isWritten")
+    public boolean isWritten() {
+        return isWritten;
+    }
 
     public static DoneReviewInfoResponse of(DoneReview doneReview) {
         return DoneReviewInfoResponse.builder()
                 .name(doneReview.getActivity().getName())
                 .imageUrl(doneReview.getDone().getImageUrl())
                 .doneDate(DateUtils.parseYearAndMonthAndDay(doneReview.getCreatedAt()))
-                .rating(doneReview.getRating())
+                .score(doneReview.getRating().getScore())
                 .content(doneReview.getContent())
                 .isWritten(doneReview.getIsWritten())
                 .point(doneReview.getActivity().getPoint())
