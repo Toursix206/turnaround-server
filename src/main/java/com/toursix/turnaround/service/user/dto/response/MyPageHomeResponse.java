793620993eaf670f633d57d7202dcd6f83fdf8f2
hyejungg <kimhyejung12@naver.com>
2023-01-10 10:16:11 +0900

    #96 [fix] 마이페이지 홈화면 조회 시 스토어 링크 넘겨주도록 추가

--- src/main/java/com/toursix/turnaround/service/user/dto/response/MyPageHomeResponse.java
+++ src/main/java/com/toursix/turnaround/service/user/dto/response/MyPageHomeResponse.java
@@ -2,6 +2,7 @@ package com.toursix.turnaround.service.user.dto.response;
 
 import com.toursix.turnaround.domain.user.OnboardingProfileType;
 import com.toursix.turnaround.domain.user.User;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,15 +17,37 @@ import lombok.ToString;
 @Builder(access = AccessLevel.PRIVATE)
 public class MyPageHomeResponse {
 
+    private final static String AOS = "AOS";
+    private final static String IOS = "iOS";
+
     private OnboardingProfileType profileType;
     private String nickname;
     private int point;
+    private StoreUrl storeUrl;
 
-    public static MyPageHomeResponse of(User user) {
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @Builder(access = AccessLevel.PRIVATE)
+    public static class StoreUrl {
+
+        private String aos;
+        private String ios;
+
+        public static StoreUrl of(String aosUrl, String iosUrl) {
+            return StoreUrl.builder()
+                    .aos(aosUrl)
+                    .ios(iosUrl)
+                    .build();
+        }
+    }
+
+    public static MyPageHomeResponse of(User user, Map<String, String> deployByOs) {
         return MyPageHomeResponse.builder()
                 .profileType(user.getOnboarding().getProfileType())
                 .nickname(user.getOnboarding().getNickname())
                 .point(user.getPoint().getAmount())
+                .storeUrl(StoreUrl.of(deployByOs.get(AOS), deployByOs.get(IOS)))
                 .build();
     }
 }
